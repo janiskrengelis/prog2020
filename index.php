a7e49fc2f45e622aff0bc5b4432f890b20609708
janiskrengelis <57032451+janiskrengelis@users.noreply.github.com>
2020-11-26 20:23:37 +0200

    Mājaslapas augšupielāde

---
+++ index.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Animal Care</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/lightbox.min.css">
+	<script type="text/javascript" src="js/lightbox-plus-jquery.min.js"></script>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<!-- Daži mainīgie var nelikties loģiski jo šai mājaslapai ātri mainīta tēma-->
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+	<a class="navbar-brand" href="images/logofull.png" data-lightbox="Header" data-title="Catgirls"><img src="images/logo.png" alt="Logo"></a>
+	<a class="navbar-brand" href="#"><h1>Animal care</h1></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarNavDropdown">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Pieejamie dzīvnieki
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+					<a class="dropdown-item" href="index.php#linkclothes">Miki</a>
+					<a class="dropdown-item" href="index.php#linklitter">Laimiņa</a>
+					<a class="dropdown-item" href="index.php#linkfeeding">Koperfīlds</a>
+					<a class="dropdown-item" href="index.php#linkgrooming">Lio</a>
+					<a class="dropdown-item" href="index.php#linkbathing">Bairons</a>
+					<a class="dropdown-item" href="index.php#linksafety">Aladins</a>
+					<a class="dropdown-item" href="index.php#linkdowntime">Džims</a>
+				</div>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="contacts.php">Contacts</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="login.php">Login</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="register.php">Register</a>
+			</li>
+		</ul>
+	</div>
+</nav>
+<!--- Image Slider -->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+	</ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="images/background1.png" alt="photo1">
+        </div>
+		<div class="carousel-item">
+			<img class="d-block w-100" src="images/background2.jpg" alt="photo2">
+		</div>
+		<div class="carousel-item">
+			<img class="d-block w-100" src="images/background3.jpg" alt="photo3">
+		</div>
+		<div class="carousel-item">
+			<img class="d-block w-100" src="images/background4.jpg" alt="photo4">
+		</div>
+		<div class="carousel-item">
+			<img class="d-block w-100" src="images/background5.jpg" alt="photo5">
+		</div>
+		<div class="carousel-item">
+			<img class="d-block w-100" src="images/background6.jpg" alt="photo6">
+		</div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+<!--- Welcome Section -->
+<div class="container-fluid padding">
+<div class="row welcome text-center">
+    <div class="col-12">
+        <h1 class="display-4">Atrodi savu īsto mīlestību! </h1>
+    </div>
+    <div class="col-12">
+        <p class="lead">Biedrība “Dzīvnieku pansija Ulubele” ir dibināta 2005.gada 21.novembrī, apvienojoties domubiedriem, kam nav vienaldzīgs dzīvnieku liktenis.</p>
+		<a id="linkclothes"></a>
+	</div>
+</div>
+</div>
+<!--- Miki transition -->
+<section class="fixed" id="clothes">
+	<div class="col-12">
+	</div>
+</section>
+<!--- Miki -->
+<div class="container-fluid padding">
+	<div class="row welcome text-center">
+		<div class="col-12">
+			<h1 class="display-1">Miki</h1>
+		</div>
+		<div class="col-12">
+			<p class="lead">“Skaistā 4 gadus vecā runčuka skatiens ir apdomīgs un modrs. Viņam ļoti nepatīk patversmē, jo citu kaķu kompānija tikai kaitina un bojā garastāvokli.
+
+Pret cilvēkiem Miki ir ļoti mīlīgs un draudzīgs, taču reizēm nespēj noslēpt savu raksturiņu – principā viņš domā, ka noteicējs attiecībās taču ir kaķis…
+
+Piemērots ģimenei, kas ir “kaķinieki” dvēselē un labi izprot tieši šādu kaķa dzīves pozīciju un kur Miki būtu vienīgais Kaķis.” 😽️
+
+🐈 Piebildīsim vien to, ka būdams vienīgais kaķis “savā Pilī=savās Mājās”, Miki jutīsies laimīgs un ar prieku dalīsies laimē ar visu ģimeni. Laimīgs Kaķis = laimīga Māja. 
+<a id="linklitter"></a>
+		</div>
+	</div>
+</div>
+<!--- Laimiņa transition -->
+<section class="fixed" id="litter">
+	<div class="col-12">
+	</div>
+</section>
+<!--- Laimiņa -->
+<div class="container-fluid padding">
+	<div class="row welcome text-center">
+		<div class="col-12">
+			<h1 class="display-1">Laimiņa</h1>
+		</div>
+		<div class="col-12">
+			<p class="lead">“Smalkā, trauslā 6 gadus vecā Laimiņa kaķenīte ir kautrīga un mazliet bikla, taču vienlaikus ļoti mierīga un prātīga.
+
+Lai atklātu savu sirsniņu un izrādītu mīlestību, Laimiņai vispirms ļoti svarīgi iepazīt un iedraudzēties ar cilvēku – svešiniekiem viņa neuzticas un skaļiem vārdiem arī nē.
+
+Vislabāk dzīvotu mierīgā vidē kopā ar ģimeni, kas ļautu Laimai pašai izvēlēties, cik daudz uzturēties sabiedrībā, cik – pavadīt klusā, mierīgā kaktiņā. Tad arī saimnieki sajutīs, ka viņas īstā labsajūta izpaužas siltā, murrājošā mīļumā pret savējiem.”
+
+🐈 Piebildīsim, ka Laimiņa ir kautrīga un pirmā svešiniekam pretī neskries, tāpēc adoptētāji viņai līdz šim pagājuši garām. Nopietnu skatienu viņa noraugās pakaļ un cer, ka kāds tomēr apstāsies, atskatīsies un pamanīs viņas kautrīgo, bet tik LIELO vēlēšanos doties uz Mājām… 
+</p>
+			<a id="linkfeeding"></a>
+		</div>
+	</div>
+</div>
+<!--- Koperfīlds transition -->
+<section class="fixed" id="feeding">
+	<div class="col-12">
+	</div>
+</section>
+<!--- Koperfīlds -->
+<div class="container-fluid padding">
+	<div class="row welcome text-center">
+		<div class="col-12">
+			<h1 class="display-1">Koperfīlds</h1>
+		</div>
+		<div class="col-12">
+			<p class="lead">“Lieliskais 6 gadus vecais Koperfīlds ieguva šo vārdu, jo sākumā patversmē pierādīja sevi kā izcilu burvju mākslinieku, kam neviens žogs, vārti vai durvis nav šķērslis uz brīvību.
+
+Tagad gan sapratis, ka izlaušanās nemaz nav tā vērta, ja patversmes voljērā vienmēr gardas pusdienas, silta miga un kopīgas pastaigas ar foršiem brīvprātīgajiem.
+
+Koperfīlds savai ģimenei būs īsts un nopietns sargs. Svešiniekus mājās noteikti neielaidīs un teritoriju kārtīgi pieskatīs bez īpaša atgādinājuma. Savējiem būs čoms un uzticamākais kompanjons visās gaitās.”
+
+🐕 Piebildīsim, ka Koperfīlda izskats pa gabalu iedveš respektu – lielais augums, ātrā reakcija un brīdinošā balss svešiniekiem liks respektēt teritorijas un saimnieku neaizskaramību. Taču savējiem Koperfīlds būs humora pilns pastaigu un spēļu biedrs – Helēnas Ertes fotogrāfijās precīzi atspoguļojas lielā suņupuikas personības smaidīgā puse. 
+</p>
+			<a id="linkgrooming"></a>
+		</div>
+	</div>
+</div>
+<!--- Lio transition -->
+<section class="fixed" id="grooming">
+	<div class="col-12">
+	</div>
+</section>
+<!--- Lio -->
+<div class="container-fluid padding">
+	<div class="row welcome text-center">
+		<div class="col-12">
+			<h1 class="display-1">Lio</h1>
+		</div>
+		<div class="col-12">
+			<p class="lead">Konkursā par iznesīgākā džentelmeņa titulu Ulubeles seniors Lio ar iesirmo bakenbārdu droši vien tālu aiz sevis atstātu vairumu jauno un daudzsološo suņu puiku. 8 gadus vecā Lio šarms reti kuru atstāj vienaldzīgu gan krāšņā kažoka, gan ieturētās uzvedības dēļ.
+
+Lio pēc dabas ir neuzbāzīgs, atturīgs un vienlaikus ļoti draudzīgs, sirsnīgs. Lieliski māk atrast kopīgu valodu ar visa vecuma bērniem – īsts bērnu draugs, iedrošinātājs un kompanjons. Ļoti tolerants pret citiem suņiem.
+
+Savu pastaigas pavadoni Lio izvadā nesteidzīgi un cēli. Vienmēr ļoti priecājas par viņam veltīto uzmanību un sarunām ar cilvēku.
+
+Tomēr ir viena lieta, kas Lio liek izkrist no atturīga džentelmeņa lomas – tā ir bumbiņa ⚽️ ! Viņam ļoti patīk spēlēties ar to un bumbiņspēle atklāj viņa patiesi priecīgo pusi – tik jautrs un dzīvespriecīgs, ka par senioru nenosauksi!
+
+Nujā, un tie kaķi… Ar kaķiem gan Lio nebūs draugos, tie viņā tikai izraisa negaidītu paātrinājumu un vēlmi padzīt kaut kur tālāk prom no acīm.
+
+Lio būs mājas mīlulis un kompanjons ikvienai ģimenei (bez kaķiem). Ideāla izvēle adopcijai tiem, kam patīk ziemeļnieki, bet nav ļoti aktīvs dzīvesveids. Lio ir līdzsvarots un lēnprātīgs, viņam nav nepieciešami ikdienā noskrieti gari kilometri, bet gan sarunām piepildītas pastaigas ar saviem cilvēkiem un suņu draugiem.
+
+Kā jau Ulubeles senioram, Lio ir sava Zelta veselības polise, kas nodrošina veterināro aprūpi uz Ulubeles rēķina līdz mūža galam.
+
+❤️ Lio ir Ulubelē kopš šī – 2020.gada pirmās dienas. Sākotnēji viņš skaitīja dienas, līdz atkal atgriezīsies mājās.
+
+Tagad jau sapratis, ka diemžēl vecajās mājās viņu vairs negaida un skaita mēnešus, līdz kāds viņu pamanīs, iemīlēs un aizvedīs uz jaunām mājām.
+
+Ļoti negribētos, lai sirmajam un sirsnīgajam džentelmenim Lio būtu jāsāk skaitīt gadi, kopš viņš nokļuvis patversmē…
+</p>
+			<a id="linkbathing"></a>
+		</div>
+	</div>
+</div>
+<!--- Bairons transition -->
+<section class="fixed" id="bathing">
+	<div class="col-12">
+	</div>
+</section>
+<!--- Bairons -->
+<div class="container-fluid padding">
+	<div class="row welcome text-center">
+		<div class="col-12">
+			<h1 class="display-1">Bairons</h1>
+		</div>
+		<div class="col-12">
+			<p class="lead">Bairons – skaists, stalts un cēlsirdīgs suns. Viņam ir 10 gadi, vieds skats uz dzīvi un tāda dzīves pieredze, par kādām raksta grāmatas. Starp citu, kā daudziem jo daudziem patversmes iemītniekiem. Bairons dzīves lielāko daļu bijis kompanjons un pavadonis savam saimniekam, kura pastāvīgā dzīvesvieta gadiem ilgi bija dārza mājiņa Bolderājas rajonā.
+
+Bairons ir ļoti draudzīgs un līdzsvarots suns. Pēc mīļā saimnieka nāves Bairons, kā iepriekšējā dzīves pieredze viņu mācījusi, neuzbāzīgi pats meklēja patvērumu un iztikšanu citur – tuvākajā apkārtnē, pie kaimiņiem un līdzjūtīgajiem. Baironu pazina daudzi apkārtējie un daudzi arī mēģināja viņam palīdzēt – pabarot, padzirdīt, bet īstu mājvietu neviens nepiedāvāja. Kādā aprīļa dienā Bairons tika atrasts bezspēcīgs, sabrucis un gandrīz izdzisis… Tad, uz rokām iecelts mašīnā, nonāca Ulubelē.
+
+Pagāja vien dažas nedēļas. Bairons atsildījās, atkopās, “atēdās”, atgulējās un uzmirdzēja kā Fēnikss no pelniem – priecīgs, ar nezūdošu smaidu mazliet sirmot sākušajā purniņā, tagad viņš joņo pa savu plašo voljēru, līksmi sasaucoties vai dusmīgi sabaroties ar apkārtējiem suņiem. Nav jau tā, ka citi suņi viņam nepatīk – tikai uztrauc! Varbūt pārāk daudz to bijis Bairona iepriekšējā dzīvē un, iespējams, turpmāk viņš priekšroku dotu mierīgākai dzīvei tikai kopā ar savu ģimeni. Cik zināms, pret kaķiem gan Baironam iebildumu nav.
+
+Patversmē labi iemācījies iet pie pavadas. Gatavs iemācīties vēl daudz ko citu, jo pāri visam ļoti ilgojas būt piederīgs un vajadzīgs savam cilvēkam.
+
+Jo tāds viņš bijis vienmēr. Uzticams un padevīgs.
+
+Īsts draugs.
+</p>
+			<a id="linksafety"></a>
+		</div>
+	</div>
+</div>
+<!--- Aladins transition -->
+<section class="fixed" id="safety">
+	<div class="col-12">
+	</div>
+</section>
+<!--- Aladins -->
+<div class="container-fluid padding">
+	<div class="row welcome text-center">
+		<div class="col-12">
+			<h1 class="display-1">Aladins</h1>
+		</div>
+		<div class="col-12">
+			<p class="lead">Mūsu brīvprātīgā Justīne raksta:
+
+“Aladins ir smuks ~2 gadus jauns suņu puika. Meklē saimnieku ar pieredzi suņu audzināšanā, vislabāk, lai viņam ir viens saimnieks, kuru pieskatīt, mīlēt un uzticēties, jo Aladins ir ļoti greizsirdīgs un, ja dzīvos ģimenē, var kaut ko nesadalīt.
+
+Un Aladinam ir bijusi slikta pieredze ar bērniem – jau pa gabalu rej un rūc uz viņiem. Vajadzīgas mājas, kur nenāktos saskarties ar bērniem. Suņu puikas Aladinam arī nepatīk, ar meitenēm varētu mēģināt sadzīvot. BET – Aladins ir draudzīgs, aktīvs un gudrs puika! Viņš prot komandas un viņam ļoti patīk bumbiņas. Labsirdīgs un draudzīgs pret cilvēkiem, ļoti vēlas būt kāda labākais draugs un acuraugs. Savējos sagaida astīti luncinot ar lielu prieku. Viņam tik ļoti vajag savu cilvēku…
+
+Lem atbildīgi un brauc ar viņu iepazīties! 🙏
+
+❤️ Ja nevari viņu adoptēt, lūdzu atbalsti Aladina un pārējo Ulubeles iemītnieku iespēju sagaidīt savu Cilvēku – pēc dažām dienām vai pēc vairākiem gadiem. Aladins patversmē gaida jau vairāk nekā 5 mēnešus. 
+</p>
+			<a id="linkplaytime"></a>
+		</div>
+	</div>
+</div>
+<!--- Džims transition -->
+<section class="fixed" id="playtime">
+	<div class="col-12">
+	</div>
+</section>
+<!--- Džims -->
+<div class="container-fluid padding">
+	<div class="row welcome text-center">
+		<div class="col-12">
+			<h1 class="display-1">Džims</h1>
+		</div>
+		<div class="col-12">
+		<a id="linkdowntime"></a>
+			<p class="lead">DŽIMS – astoņgadīgs jauneklis. Kaut arī pēc pases par jaunekli īsti vairs nav saucams, Džima dzīvesprieks, kustīgums un atsperīgums liek domāt par jaunības eliksīra esamību vai vismaz par lieliskiem sportista gēniem.
+Divus ar pusi gadus nepelnīti nepamanīts patversmes voljērā, skaistais, sportiskais Džims ļoti noilgojies pēc mājām, draudzības un cilvēku sabiedrības.
+
+Kā jau katram no mums, Džimim ir sava gaišā un tumšā puse jeb lietas, ar ko lepojamies un lietas, ko labāk gribētos paslaucīt zem paklājiņa. Sāksim ar tām otrajām. Uzreiz jāteic, ka Džims ir augstlēcējs – “paņems” jebkuru žogu, sētu, mūri. Nevis tāpēc, lai izlauztos un aizmuktu, bet tāpēc, ka sportisks azarts pārbaudīt sevi un augstumu. Un ja vēl žoga otrā pusē ir kāds sugas brālis, tad azarts pieaug trīskārt. Jo otra Džima “slidenā” lieta ir nīgrums pret citiem suņiem un vēlme parādīt savu pārākumu. Kas zina, kādēļ dzīve Džimim to iemācījusi – iespējams, pats kādreiz bijis upura lomā.
+
+Taču mēs gribam, lai šī godīgi atklātā Džima tumšā puse “nenoraktu” viņa iespēju atrast mājas. Vai ar to iespējams sadzīvot? Jā, protams, ja ir atbilstoši apstākļi!
+1) Dzīvoklī vai iekštelpās nav kārdinājuma “paņemt”augstumu, tāpat arī voljērā ar jumtu (sietu). Iekštelpās Džims māk kārtīgi uzvesties un dzīvos labprāt, ja būs ikdienas garās pastaigas un sportiskas aktivitātes. Pie pavadas iet ar prieku, uzvedas rātni un pieskaņojas cilvēkam. Taču uzskata par savu pienākumu pieskatīt un sargāt savējos – gan teritorijā, gan pastaigas laikā – tā, viņaprāt, izpaužas uzticība.
+2) Būtu vēlams, lai Džimim visu laiku acu priekšā nebūtu citi sugasbrāļi – tas kaitina un liek vicināt dūres, parādīt savu spēku. Mierīgos apstākļos Džims arī pats būs mierīgs un mūsu kinologs Ingus jaunajam saimniekam un Džimim labprāt pasniegs kopīgas pieklājīgas uzvedības stundas. Starp citu, pret draudzīgām suņu meitenēm Džims tīri labi māk izturēties kā džentlmenis, tomēr labāk, ja viņš būs vienīgais suns ģimenē.
+Visādi citādi – Džims ir fantastisks draugs un sabiedrotais spēlēs un ikdienas gaitās. Cilvēkus, kurus iepazīst vairāk par vienu reizi, mīl un seko uz soļa. Azartiskums ir rakstura īpašība, tāpēc tas izpaužas ne tikai augstlēkšanā, bet arī bumbiņspēlē, pēdu meklēšanā, rotaļās, skriešanas sacensībās, samīļošanā un visās citās jautrībās, ko kopīgi var darīt suns un cilvēks. Ja vien cilvēkam vajadzīgs suns. Tik foršs suns kā Džims.
+
+Sajutis sev pievērstu uzmanību, Džims atplaukst smaidā un liek smaidīt arī mums.
+Viņš gaida, kad varēs uzsmaidīt savam saimniekam. Un Tu?
+</p>
+		</div>
+	</div>
+</div>
+<!--- Footer -->
+<footer>
+	<a id="linkcontacts"></a>
+	<div class="container-fluid padding">
+	<div class="row text-center">
+    	<div class="col-lg-4">
+        	<h3>Our location</h3>
+        	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4351.19249131942!2d24.11177918303782!3d56.95571847305823!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x96a8a0e931b27448!2sR%C4%ABgas%20Valsts%20Tehnikums!5e0!3m2!1slv!2slv!4v1575641566597!5m2!1slv!2slv" width="400" height="350" style="border:0;" allowfullscreen=""></iframe>
+    	</div>
+    	<div class="col-lg-4">
+        	<h3>Our contacts</h3>
+        	<p>+371 12341234</p>
+        	<p>nooneLooks@this.com</p>
+        	<p>100 Street Name</p>
+        	<p>City, LV-1000</p>
+			<a href="http://ulubele.org/category/mekle-majas/">Information source</a><br>
+			<a href="https://github.com/janiskrengelis/prog2020">Github repository</a>
+		</div>
+		<div class="col-lg-4">
+        	<h3>A short video about us</h3>
+        	<iframe width="400" height="250" src="https://www.youtube.com/embed/Y5ygS16408w" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    	</div>
+	</div>
+	</div>
+</footer>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Animal Care</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/lightbox.min.css">
+	<script type="text/javascript" src="js/lightbox-plus-jquery.min.js"></script>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+	<a class="navbar-brand" href="images/logofull.png" data-lightbox="Header" data-title="Catgirls"><img src="images/logo.png" alt="Logo"></a>
+	<a class="navbar-brand" href="#"><h1>Animal care</h1></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarNavDropdown">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Pieejamie dzīvnieki
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+					<a class="dropdown-item" href="index.php#linkclothes">Miki</a>
+					<a class="dropdown-item" href="index.php#linklitter">Laimiņa</a>
+					<a class="dropdown-item" href="index.php#linkfeeding">Koperfīlds</a>
+					<a class="dropdown-item" href="index.php#linkgrooming">Lio</a>
+					<a class="dropdown-item" href="index.php#linkbathing">Bairons</a>
+					<a class="dropdown-item" href="index.php#linksafety">Aladins</a>
+					<a class="dropdown-item" href="index.php#linkdowntime">Džims</a>
+				</div>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="contacts.php">Contacts</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="login.php">Login</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="register.php">Register</a>
+			</li>
+		</ul>
+	</div>
+</nav>
+
+<!-- saturs -->
+<section class="fixed" id="grooming">
+	<div class="col-12">
+	</div>
+</section>
+<div class="container-fluid padding">
+	<div class="row welcome text-center">
+		<div class="col-12">
+			<h1 class="display-1">Register</h1>
+		</div>
+		<fieldset class="form-group">
+
+            <div class="form-group">
+                <label class="form-control-label" for="username">Username</label>
+                <input  class="form-control" type="text" name="username" id="username" placeholder="Your Username" minlength="1" required>
+                <span><?php if (isset($errors['username1'])) echo $errors['username1'];?></span>
+            </div><!-- form-group -->
+
+            <div class="form-group">
+                <label for="pwd">Password:</label><br>
+                <input class="form-control" type="password" name="pwd" id="pwd" placeholder="Password" minlength="8" required>
+                <span><?php if (isset($errors['pwd1'])) echo $errors['pwd1'];?>
+            </div><!-- form-group -->
+
+            <div class="form-group">
+                <label class="form-control-label" for="email">Email</label>
+                <input class="form-control" type="email" name="email" id="email" placeholder="Address" required>
+                <span><?php if (isset($errors['email1'])) echo $errors['email1'];?>
+            </div><!-- form-group -->
+            <div class="form-group">
+                <label class="form-control-label" for="name">Name</label>
+                <input  class="form-control" type="text" name="name" id="name" placeholder="Your Name" >
+                <span><?php if (isset($errors['name1'])) echo $errors['name1'];?>
+            </div><!-- form-group -->
+            <div class="form-group">
+                <label class="form-control-label" for="surname">Surname</label>
+                <input  class="form-control" type="text" name="surname" id="surname" placeholder="Your Surname" >
+                <span><?php if (isset($errors['surname1'])) echo $errors['surname1'];?>
+            </div><!-- form-group -->
+			<label class="form-control-label" for="image">Image</label>
+            <input type="file" name="myFile" name="filename">
+            <button class="btn btn-primary" type="submit">Submit</button>
+        </fieldset>
+		<?php
+
+//not empty
+
+if ($_POST) {
+    $errors = array();
+
+//start validation
+    if(empty($_POST['username'])){
+        $errors['username1'] = "Your username cannot be empty";
+    }
+    if(empty($_POST['pwd'])){
+        $errors['pwd1'] = "Your password cannot be empty";
+    }
+    if(empty($_POST['email'])){
+        $errors['email1'] = "Your email cannot be empty";
+    }
+    if(empty($_POST['name'])){
+        $errors['name1'] = "Your name cannot be empty";
+    }
+    if(empty($_POST['surname'])){
+        $errors['surname1'] = "Your surname cannot be empty";
+    }
+//check errors
+    if (count($errors) == 0) {
+        //redirect to success page
+        header("Location: success.php");
+        exit();
+    }
+}
+
+		?>
+	</div>
+</div>
+<!--- Footer -->
+<footer>
+	<a id="linkcontacts"></a>
+	<div class="container-fluid padding">
+	<div class="row text-center">
+    	<div class="col-lg-4">
+        	<h3>Our location</h3>
+        	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4351.19249131942!2d24.11177918303782!3d56.95571847305823!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x96a8a0e931b27448!2sR%C4%ABgas%20Valsts%20Tehnikums!5e0!3m2!1slv!2slv!4v1575641566597!5m2!1slv!2slv" width="400" height="350" style="border:0;" allowfullscreen=""></iframe>
+    	</div>
+    	<div class="col-lg-4">
+        	<h3>Our contacts</h3>
+        	<p>+371 12341234</p>
+        	<p>nooneLooks@this.com</p>
+        	<p>100 Street Name</p>
+        	<p>City, LV-1000</p>
+			<a href="http://ulubele.org/category/mekle-majas/">Information source</a><br>
+			<a href="https://github.com/janiskrengelis/prog2020">Github repository</a>
+		</div>
+		<div class="col-lg-4">
+        	<h3>A short video about us</h3>
+        	<iframe width="400" height="250" src="https://www.youtube.com/embed/Y5ygS16408w" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    	</div>
+	</div>
+	</div>
+</footer>
+</body>
+</html>
